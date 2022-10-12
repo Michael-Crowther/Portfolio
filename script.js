d6f49f98a99d11778fff0486d93c7a8211064ef2
Michael-Crowther <67437978+Michael-Crowther@users.noreply.github.com>
2022-10-12 16:09:26 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//The Modal functionality
+///////////////////The Modal functionality
 var modal = document.getElementById("myModal");
 
     // Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -25,7 +25,34 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-//End modal functionality
+////////////////End modal functionality
+
+
+
+
+////////////////Sticky navbar
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+/////////////////End sticky nabar
+
+
+
+
 
 
 window.addEventListener('load', function() {
