fc66d9810f128f59618b028ba271b25b969feb11
Michael-Crowther <md.crowther@yahoo.com>
2025-03-03 13:51:50 -0700

    added standalone output

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // Helps with deployments
 };
 
 export default nextConfig;
