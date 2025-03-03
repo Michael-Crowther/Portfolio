fd101a584e784cb6db1d833c0a00589481fe4379
Michael-Crowther <md.crowther@yahoo.com>
2025-03-03 13:34:50 -0700

    Fixed padding issue on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen w-full flex">
       <span className="w-1/6 hidden 2xl:block" />
-      <div className="w-full flex flex-col 2xl:flex-row px-6 md:px-10 2xl:px-0">
+      <div className="w-full flex flex-col 2xl:flex-row 2xl:px-0">
         <section className="border border-pink-500 w-full h-full p-16 2xl:min-w-[600px] 2xl:w-1/3">
           <p className="text-[55px] font-bold">Michael Crowther</p>
           <p className="text-[20px]">Full Stack Engineer</p>
